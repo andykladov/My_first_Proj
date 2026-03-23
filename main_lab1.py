5f0b5e6905f62fde089080952ef549a09474d7a4
andykladov <andykladov@gmail.com>
2026-03-23 17:37:35 +0300

    My_firs_Lesson

---
+++ main_lab1.py
@@ -0,0 +1,22 @@
+print("Мир")
+age=16
+print('Привет! Мне',age,'лет')
+print(type(age))
+
+price=308.25 #Цена сыра
+print('Сыр стоит ', price,'Рублей')
+print(type(price))
+"""
+www
+"""
+MyName='Andy'
+print(type(MyName))
+print('Привет, я ', MyName)
+
+age=input('How old are you')
+print('I am ',age,'years old')
+print(type(age))
+age=int(age)
+print(type(age))
+
+
