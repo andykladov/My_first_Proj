5c8668126b58f87ce0f6e00bfdf480b17de0a48a
andykladov <andykladov@gmail.com>
2026-03-24 22:37:56 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main_lab1.py

--- main_lab2.py
+++ main_lab2.py
@@ -56,11 +56,13 @@ print(a)
 a={1,1,1,2,2,3,5,8}
 print(a)
 print(type(a))
-
-a={'Яблоки','Груши','Персики'}
-b={'Груши','Фйва','Персики','T'}
+"""
+"""
+a={'Яблоки','Груши','Персики','Фйва'}
+b={'Груши','Фйва','Персики','Tыква'}
 print(a.union(b))
 print(a.intersection(b))
+"""
 """
 countries = ['India', 'Brazil', 'China', 'Nigeria', 'Bangladesh', 'U.S.', 'Russia', 'Pakistan', 'Mexico']
 populations = [1326801576, 209567920, 1382323332, 186987563, 162910864, 324118787, 143439832, 192826502, 128632004]
@@ -68,6 +70,41 @@ print(dict(zip(countries, populations)))
 
 from operator import itemgetter
 print(sorted(populations_by_countries.items(), key=itemgetter(1),reverse=True))
+"""
+"""
+import numpy as np
+a=[1,2,3,4,5,6]
+b=np.array(a)
+print(b)
+print(type(b))
 
+print(a*3)
+print(b*3)
+"""
+"""
+def f(x):
+   y=x**2
+   return y
+
+print()
+y=f(x)
+print(y)
+
+import matplotlib.pyplot as plt
+plt.plot(x.y)
+plt.show()
+"""
+"""
+import numpy as np
+x=np.linspace(-1,1,100)
+
+a=-1
+b=1
+N=100
+delta_x=(b-a)/N
+integral=delta_x*sum(y)
+print('Integral = ',integral)
+
+"""
 
 
